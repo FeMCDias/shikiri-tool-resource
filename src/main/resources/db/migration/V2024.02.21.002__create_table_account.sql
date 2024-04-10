d0478eee682b52fd177f5dacad5ca248a98d8c40
FeMCDias <femcdias@gmail.com>
2024-04-10 02:31:29 -0300

    Add tx_userId to sql

--- src/main/resources/db/migration/V2024.02.21.002__create_table_account.sql
+++ src/main/resources/db/migration/V2024.02.21.002__create_table_account.sql
@@ -4,5 +4,6 @@ CREATE TABLE tool
     tx_name character varying(256) NOT NULL,
     tx_category character varying(128) NOT NULL,
     tx_description character varying(512) NOT NULL,
+    tx_userId character varying(36) NOT NULL,
     CONSTRAINT tool_pkey PRIMARY KEY (id_tool)
 );
